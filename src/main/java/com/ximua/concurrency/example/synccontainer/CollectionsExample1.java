35421fb998f1793fbb2ea2b80d12bec7dd6bf853
qiankeqin <951669478@qq.com>
2018-04-26 22:02:51 +0800

    同步容器

---
+++ src/main/java/com/ximua/concurrency/example/synccontainer/CollectionsExample1.java
@@ -0,0 +1,57 @@
+package com.ximua.concurrency.example.synccontainer;
+
+import com.ximua.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class CollectionsExample1 {
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行到线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private static List<Integer> list = Collections.synchronizedList(new ArrayList<>()) ;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i = 0;i < clientTotal; i++){
+            final int count = i;
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    addList(count);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}",list.size());
+
+    }
+
+
+    public static void addList(int i){
+        list.add(i);
+    }
+}
---
+++ src/main/java/com/ximua/concurrency/example/synccontainer/CollectionsExample2_SET.java
@@ -0,0 +1,54 @@
+package com.ximua.concurrency.example.synccontainer;
+
+import com.ximua.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class CollectionsExample2_SET {
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行到线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private static Set<Integer> list = Collections.synchronizedSet(new HashSet<>()) ;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i = 0;i < clientTotal; i++){
+            final int count = i;
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    addList(count);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}",list.size());
+
+    }
+
+
+    public static void addList(int i){
+        list.add(i);
+    }
+}
