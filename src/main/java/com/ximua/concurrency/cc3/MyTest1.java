89c3e0872f21ee738154f098e8a32c54cf76a24a
qiankeqin <qiankeqin@51xf.cn>
2020-07-02 17:32:24 +0800

    synchronized

--- src/main/java/com/ximua/concurrency/cc3/MyTest1.java
+++ src/main/java/com/ximua/concurrency/cc3/MyTest1.java
@@ -6,4 +6,18 @@ package com.ximua.concurrency.cc3;
  * @date 2020-07-01 08:45
  */
 public class MyTest1 {
+    private Object obj = new Object();
+
+    public void method() {
+        synchronized (obj){
+            System.out.println("hello world");
+            throw new RuntimeException();
+        }
+    }
+
+    public void method2(){
+        synchronized (obj){
+            System.out.println("welcome");
+        }
+    }
 }
